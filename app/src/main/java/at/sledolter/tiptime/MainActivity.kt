1158a637025fe89897b86a3315d9cb7c41cd18c0
sledolter <s.ledolter@gmail.com>
2021-12-11 10:10:57 +0100

    SL: added scrollview; added primaray color; changed tint of icons; added hiding of softkeyboard on enter

--- app/src/main/java/at/sledolter/tiptime/MainActivity.kt
+++ app/src/main/java/at/sledolter/tiptime/MainActivity.kt
@@ -1,7 +1,11 @@
 package at.sledolter.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import at.sledolter.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -14,6 +18,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(
+                view,
+                keyCode
+            )
+        }
     }
 
     private fun calculateTip() {
@@ -38,5 +48,15 @@ class MainActivity : AppCompatActivity() {
     private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
